44febaa191114a87d52b1f89558c40f33a624687
John Dillon <41346111+jd3builds@users.noreply.github.com>
2020-10-17 03:13:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ else:
 
 setuptools.setup(
     name="BA-produce-tracker",
-    version="0.0.3",
+    version="0.0.4",
     author="Brody, Joseph // Dillon, John // Estrada, Pablo // Todd, Alexis // Garcia, Marissa",
     url="https://github.com/jd3builds/BadApplesProject",
     description="Produce expiration tracker",
