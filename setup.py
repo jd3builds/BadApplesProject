5d44bab32af2dbb2584b219a361c553ec99aabb6
John Dillon <41346111+jd3builds@users.noreply.github.com>
2020-10-17 02:46:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,30 @@
 import setuptools
 import os
+import platform
 
-folder = os.path.dirname(os.path.realpath(__file__))
-req_path = folder + '/requirements.txt'
-install_requires = []
+if platform.system == 'Windows':
 
-if os.path.isfile(req_path):
-    with open(req_path) as file:
-        install_requires = file.read().splitlines()
+    folder = os.path.dirname(os.path.realpath(__file__))
+    req_path = folder + '/requirements.txt'
+    install_requires = []
+
+    if os.path.isfile(req_path):
+        with open(req_path) as file:
+            install_requires = file.read().splitlines()
+else:
+    folder = os.path.dirname(os.path.realpath(__file__))
+    req_path = folder + '/linuxrequirements.txt'
+    install_requires = []
+
+    if os.path.isfile(req_path):
+        with open(req_path) as file:
+            install_requires = file.read().splitlines()
 
 setuptools.setup(
     name="BA-produce-tracker",
-    version="0.0.1",
+    version="0.0.2",
     author="Brody, Joseph // Dillon, John // Estrada, Pablo // Todd, Alexis // Garcia, Marissa",
+    url="https://github.com/jd3builds/BadApplesProject",
     description="Produce expiration tracker",
     python_requires='>=3.8',
     install_requires=install_requires
